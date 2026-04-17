0c2491ae5f875ee233d3e30567b173cd5b2c6ad2
yehyune0201 <yehyune0201@gmail.com>
2026-04-17 23:35:22 +0900

    스테이지3 숲

--- GameEngine testttt/Assets/Script/PlayerController.cs
+++ GameEngine testttt/Assets/Script/PlayerController.cs
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            pAni.SetTrigger("Jump");
+            
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
