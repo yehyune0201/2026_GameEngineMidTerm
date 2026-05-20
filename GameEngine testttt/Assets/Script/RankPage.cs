fa624c827bc22c66d2a5bcc5f0afc942ccedea7b
yehyune0201 <yehyune0201@gmail.com>
2026-05-20 13:29:38 +0900

    랭킹 완성

--- GameEngine testttt/Assets/Script/RankPage.cs
+++ GameEngine testttt/Assets/Script/RankPage.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using UnityEngine;
 using TMPro;
+
 public class RankPage : MonoBehaviour
 {
     [SerializeField] Transform contentRoot;
@@ -8,24 +9,65 @@ public class RankPage : MonoBehaviour
 
     StageResultList allData;
 
+    int currentStage = 1;
+
     void Awake()
     {
         allData = StageResultSaver.LoadRank();
         RefreshRankList();
     }
+
     void RefreshRankList()
     {
         foreach (Transform child in contentRoot)
         {
             Destroy(child.gameObject);
         }
-        var sortedData = allData.results.Where(r => r.stage == 1).OrderByDescending(x => x.score).ToList();
 
-         for (int i = 0; i < sortedData.Count; i++)
-         {
+        var sortedData = allData.results
+            .Where(r => r.stage == currentStage)
+            .OrderByDescending(x => x.score)
+            .ToList();
+
+        for (int i = 0; i < sortedData.Count; i++)
+        {
             GameObject row = Instantiate(rowPrefabs, contentRoot);
-            TMP_Text rankText = row.GetComponentInChildren<TMP_Text>();
-            rankText.text = $"{i + 1}. {sortedData[i].playerName} - {sortedData[i].score}";
-         }
+
+            TMP_Text rankText =
+                row.GetComponentInChildren<TMP_Text>();
+
+            rankText.text =
+                $"{i + 1}. {sortedData[i].playerName} - {sortedData[i].score}";
+        }
+    }
+
+    public void Stage1Button()
+    {
+        currentStage = 1;
+        RefreshRankList();
+    }
+
+    public void Stage2Button()
+    {
+        currentStage = 2;
+        RefreshRankList();
+    }
+
+    public void Stage3Button()
+    {
+        currentStage = 3;
+        RefreshRankList();
+    }
+
+    public void Stage4Button()
+    {
+        currentStage = 4;
+        RefreshRankList();
+    }
+
+    public void Stage5Button()
+    {
+        currentStage = 5;
+        RefreshRankList();
     }
 }
